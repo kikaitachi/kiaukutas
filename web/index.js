029480a56ea5403eb78714404e36dffa134191be
Mykolas Juraitis <mykolas@engineeredarts.co.uk>
2024-05-04 16:36:26 +0100

    feat: enable antialiasing

--- web/index.js
+++ web/index.js
@@ -6,7 +6,9 @@ import URDFLoader from "https://esm.sh/urdf-loader@0.12.1";
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+  antialias: true,
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
