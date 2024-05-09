e3ba209780ad159fae2b94f7fbeedd9f540fb4b1
Mykolas Juraitis <mykolas@engineeredarts.co.uk>
2024-05-09 20:34:03 +0100

    build: install FreeCAD addon for gears

--- build.sh
+++ build.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+freecad_gears_dir=~/.local/share/FreeCAD/Mod/freecad.gears
+
+if [ ! -d "${freecad_gears_dir}" ]; then
+  git clone --depth=1 https://github.com/looooo/freecad.gears.git "${freecad_gears_dir}"
+fi
+
 build_dir=build
 if [ ! -d "${build_dir}" ]; then
   cmake -B "${build_dir}" -G Ninja
